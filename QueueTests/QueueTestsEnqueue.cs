fb2cfbcc8d63c66cd894a658ba25d1c6a3fb2b66
annyrtd <fyz95@mail.ru>
2015-07-12 18:05:19 +0300

    Unit tests chanded.
    Enqueue method tests changed.
    Dequeue method test added.

--- QueueTests/QueueTestsEnqueue.cs
+++ QueueTests/QueueTestsEnqueue.cs
@@ -7,7 +7,7 @@ using Queue;
 namespace QueueTests
 {
     [TestClass]
-    public class QueueTests
+    public class QueueTestsEnqueue
     {
         [TestMethod]
         public void Enqueue_NextIndexLessThanArrayLength()
@@ -24,7 +24,7 @@ namespace QueueTests
         }
 
         [TestMethod]
-        public void Enqueue_NextIndexGreaterThanArrayLength()
+        public void Enqueue_NextIndexGreaterThanArrayLength_ResizeArray()
         {
             // arrange
             Queue<int> expected = new Queue<int>(1, 2, 3, 4, 5, 6);
@@ -35,20 +35,7 @@ namespace QueueTests
 
             // assert
             Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Enqueue_NextIndexGreaterThanArrayLength_ResizeArray()
-        {
-            // arrange
-            Queue<int> actual = new Queue<int>(1, 2, 3, 4, 5);
-            int expected = 10;
-
-            // act
-            actual.Enqueue(6);
-
-            // assert
-            Assert.AreEqual(expected, actual.Items.Length);
+            Assert.AreEqual(10, actual.Items.Length);
         }
     }
 }
